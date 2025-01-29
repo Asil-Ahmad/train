568c5a734b65227275e52d64b32003c1bf5b06ac
Asil Ahmad <identity101@duck.com>
2025-01-29 11:55:50 +0530

    Add initial project structure with database connection and layout components

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+include('constant/header.html');
+
+?>
+
+<body class="flex flex-row">
+    <?php
+    include('constant/sidebar.html');
+    ?>
+<h1>This is Dashboard</h1>
+
+</body>
+
+
+</html>
---
+++ src/components/AddTrains.php
@@ -0,0 +1,12 @@
+<div class="flex">
+
+    <?php
+    include('../../constant/header.html');
+    include('../../constant/sidebar.html');
+    ?>
+
+    <div class="w-[80%] z-50 bg-gray-200">
+        <h1>You can add train here</h1>
+    </div>
+
+</div>
