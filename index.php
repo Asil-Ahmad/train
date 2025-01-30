8e6122a1690ef47154a95c247c26dbd116977d31
Asil Ahmad <identity101@duck.com>
2025-01-30 12:19:26 +0530

    Add SeatAvailable component, update header with Lottie animations, and enhance sidebar styling

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
 
 <?php
 include('constant/header.html');
-
+include('constant/sidebar.html');
 ?>
 
-<body class="flex flex-row">
-    <?php
-    include('constant/sidebar.html');
-    ?>
-<h1>This is Dashboard</h1>
-
+<body class="bg-[#F5F5F5] h-full min-h-screen">
+    <div class="relative flex flex-col  justify-center items-center py-20">
+        <h1 class="text-4xl uppercase tracking-wider font-medium text-center w-full z-10 scale-y-90">Online railway reservation system</h1>
+        <lottie-player src="/train/assets/QHudXC24B8.json" background="transparent" speed="1" style="width: 300px; height: 300px;" class="absolute left-0"  autoplay></lottie-player>
+        <lottie-player src="/train/assets/QHudXC24B8.json" background="transparent" speed="1" style="width: 300px; height: 300px;" class="absolute right-0 scale-x-[-1]" autoplay></lottie-player>
+    </div>
 </body>
 
 
